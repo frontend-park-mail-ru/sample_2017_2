1402ff9b64514e5b15a85d7c0b8b14c387e25274
Ostapenko Anatoliy <a.ostapenko@corp.mail.ru>
2017-09-08 17:57:19 +0300

    Test

--- public/main.js
+++ public/main.js
@@ -1,7 +1,51 @@
 'use strict';
 
+//
+// function auth(username, email, callback) {
+// 	const user = {username, email};
+// 	const body = JSON.stringify(user);
+//
+// 	const xhr = new XMLHttpRequest();
+// 	xhr.open('POST', '/auth', true);
+// 	xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+// 	xhr.withCredentials = true;
+// 	xhr.onreadystatechange = function () {
+// 		if (xhr.readyState !== 4) return;
+// 		if (+xhr.status !== 200) {
+// 			return callback(xhr);
+// 		}
+// 		const response = JSON.parse(xhr.responseText);
+// 		callback(null, response);
+// 	};
+// 	xhr.send(body);
+// }
+//
+// function whoami(callback) {
+// 	const xhr = new XMLHttpRequest();
+// 	xhr.open('GET', '/me', true);
+// 	xhr.withCredentials = true;
+// 	xhr.onreadystatechange = function () {
+// 		if (xhr.readyState !== 4) return;
+// 		if (+xhr.status !== 200) {
+// 			return callback(xhr);
+// 		}
+// 		const response = JSON.parse(xhr.responseText);
+// 		callback(null, response);
+// 	};
+// 	xhr.send();
+// }
+//
+// auth('sawoder', 'a.ostapenko@corp.mail.ru', (err, res) => console.log({err, res}));
+//
+//
+// setTimeout(() => {
+// 	whoami((err, res) => console.log({err, res}));
+// }, 3000);
+
 const greeting = document.getElementsByClassName('greeting')[0];
 
 greeting.addEventListener('click', () => {
 	greeting.classList.add('hidden');
 }, false);
+
+// console.log(document.cookie);
--- server/index.js
+++ server/index.js
@@ -1,22 +1,52 @@
 'use strict';
 
 const express = require('express');
+const body = require('body-parser');
+const cookie = require('cookie-parser');
 const morgan = require('morgan');
+const uuid = require('uuid/v4');
 const app = express();
 
 
 app.use(morgan('dev'));
 app.use(express.static('public'));
+app.use(body.json());
+app.use(cookie());
 
 
 const users = {};
+const ids = {};
 
 app.post('/auth', function (req, res) {
+	const username = req.body.username;
+	const email = req.body.email;
+	if (!username || !email) {
+		return res.status(400).end();
+	}
+	if (!users[email]) {
+		users[email] = {
+			username,
+			email,
+			count: 0,
+		};
+	}
+	const id = uuid();
+	ids[id] = email;
 
+	res.cookie('podvorot', id, {domain: 'localhost', expires: new Date(Date.now() + 1000 * 60 * 10)});
+	res.json({id});
 });
 
 app.get('/me', function (req, res) {
+	const id = req.cookies['podvorot'];
+	const email = ids[id];
+	if (!email || !users[email]) {
+		return res.status(401).end();
+	}
 
+	users[email].count += 1;
+
+	res.json(users[email]);
 });
 
 const port = process.env.PORT || 3000;
